212ae519cf7fc0e75b0c5b73b06ab9902062d0a2
SONU KUMAR SINGH <sonukrr526@gmail.com>
2026-01-13 00:25:16 +0530

    Migrating to Dtos

--- src/main/java/com/app/ecom/controller/UserController.java
+++ src/main/java/com/app/ecom/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.app.ecom.controller;
 
 import com.app.ecom.bo.User;
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
 import com.app.ecom.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,14 +32,31 @@ public class UserController {
 //    public List<User> getAllUser(){
 //        return userService.fetchAllUser();
 //    }
-    @GetMapping("/api/users")
+    /*@GetMapping("/api/users")
     public ResponseEntity<List<User>> getAllUser(){
+        return new ResponseEntity<>(userService.fetchAllUser(), HttpStatus.OK);
+        //return ResponseEntity.ok(userService.fetchAllUser());
+    }*/
+    @GetMapping("/api/users")  //by using dto
+    public ResponseEntity<List<UserResponse>> getAllUser(){
         return new ResponseEntity<>(userService.fetchAllUser(), HttpStatus.OK);
         //return ResponseEntity.ok(userService.fetchAllUser());
     }
 
-    @GetMapping("/api/users/{id}")
+   /* @GetMapping("/api/users/{id}")
     public ResponseEntity<User> getAllUser(@PathVariable Long id){
+//        User user = userService.fetchAllUser(id);
+//        if(user==null)
+//            return ResponseEntity.notFound().build();
+//            return  ResponseEntity.ok(user);
+        //using steram
+        return userService.fetchUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.notFound().build());
+    }*/
+
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<UserResponse> getAllUser(@PathVariable Long id){
 //        User user = userService.fetchAllUser(id);
 //        if(user==null)
 //            return ResponseEntity.notFound().build();
@@ -54,17 +73,31 @@ public class UserController {
 //        userList.add(user);
 //        return userList;
 //    }
-    @PostMapping("/api/users")
+    /*@PostMapping("/api/users")
     public ResponseEntity<String> createUser(@RequestBody User user){
         userService.addUser(user);
         return ResponseEntity.ok("User Added Succesfully");
-    }
+    }*/
+    //using dto
+@PostMapping("/api/users")
+public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest){
+    userService.addUser(userRequest);
+    return ResponseEntity.ok("User Added Succesfully");
+}
 
-    @PutMapping("/api/users/{id}")
+    /*@PutMapping("/api/users/{id}")
     public ResponseEntity<String> createUser(@RequestBody User updatedUser, @PathVariable Long id){
         boolean updated = userService.updateUser(updatedUser, id);
         if(updated)
         return ResponseEntity.ok("User Updated Succesfully");
+        return ResponseEntity.notFound().build();
+    }*/
+
+    @PutMapping("/api/users/{id}")
+    public ResponseEntity<String> createUser(@RequestBody UserRequest updatedUserRequest, @PathVariable Long id){
+        boolean updated = userService.updateUser(updatedUserRequest, id);
+        if(updated)
+            return ResponseEntity.ok("User Updated Succesfully");
         return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/app/ecom/service/UserService.java
+++ src/main/java/com/app/ecom/service/UserService.java
@@ -1,6 +1,10 @@
 package com.app.ecom.service;
 
+import com.app.ecom.bo.Address;
 import com.app.ecom.bo.User;
+import com.app.ecom.dto.AddressDto;
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
 import com.app.ecom.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +21,17 @@ public class UserService {
     private final UserRepository userRepository;
     Long nextId = 1L;
 
-    public List<User> fetchAllUser(){
+    /*public List<User> fetchAllUser(){
 
         //return userList;
         return userRepository.findAll();
+    }*/
+    public List<UserResponse> fetchAllUser(){
+
+        //return userList;
+        return userRepository.findAll().stream()
+                .map(this::mapToUserResponse)
+                .collect(Collectors.toList());
     }
 
 
@@ -27,11 +39,43 @@ public class UserService {
 //        userList.add(user);
 //        return userList;
 //    }
-    public void addUser(User user){
+    /*public void addUser(User user){
 //        user.setId(nextId++);
 //        userList.add(user);
         userRepository.save(user);
+    }*/
+    //using dto
+public void addUser(UserRequest userRequest){
+//        user.setId(nextId++);
+//        userList.add(user);
+    //User user =mapToUser(userRequest);  this is added by my logic
+    User user = new User();
+    updateUserFromRequest(user, userRequest);
+    userRepository.save(user);
+}
+
+    private void updateUserFromRequest(User user, UserRequest request) {
+
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setPhone(request.getPhone());
+        //user.setRole(request.getRole());
+
+        if (request.getAddress() != null) {
+
+            Address address = new Address();
+
+            address.setCountry(request.getAddress().getCountry());
+            address.setState(request.getAddress().getState());
+            address.setCity(request.getAddress().getCity());
+            address.setStreet(request.getAddress().getStreet());
+            address.setZipcode(request.getAddress().getZipcode());
+
+            user.setAddress(address);
+        }
     }
+
 
 //    public User fetchAllUser(Long id) {
 //        for(User user: userList){
@@ -42,9 +86,13 @@ public class UserService {
 //    }
 //}
 
-    public Optional<User> fetchUser(Long id) {
+    /*public Optional<User> fetchUser(Long id) {
         //return userList.stream().filter(user -> user.getId().equals(id)).findFirst();
         return userRepository.findById(id);
+    }*/
+    public Optional<UserResponse> fetchUser(Long id) {
+        //return userList.stream().filter(user -> user.getId().equals(id)).findFirst();
+        return userRepository.findById(id).map(this::mapToUserResponse);
     }
 
 //    public User updateUser(User user, Long id) {
@@ -58,7 +106,7 @@ public class UserService {
 //        return null;
 //    }
 
-    public boolean updateUser(User user, Long id) {
+    /*public boolean updateUser(User user, Long id) {
 //        return userList.stream()
 //                .filter(user1 -> user1.getId().equals(id))
 //                .findFirst().map(existingUser->{
@@ -73,5 +121,68 @@ public class UserService {
                     userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
+    }*/
+
+    public boolean updateUser(UserRequest updateUserRequest, Long id) {
+//        return userList.stream()
+//                .filter(user1 -> user1.getId().equals(id))
+//                .findFirst().map(existingUser->{
+//                    existingUser.setFirstName(user.getFirstName());
+//                    existingUser.setLastName(user.getLastName());
+//                    return true;
+//                }).orElse(false);
+        return userRepository.findById(id)
+                .map(existingUser->{
+                    updateUserFromRequest(existingUser, updateUserRequest);
+                    userRepository.save(existingUser);
+                    return true;
+                }).orElse(false);
+    }
+
+    private User mapToUser(UserRequest request) {
+        User user = new User();
+
+        // If you use this for update, set ID also
+        // user.setId(request.getId());
+
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setPhone(request.getPhone());
+        //user.setRole(request.getRole());
+
+        if (request.getAddress() != null) {
+            Address address = new Address();
+            address.setCountry(request.getAddress().getCountry());
+            address.setState(request.getAddress().getState());
+            address.setCity(request.getAddress().getCity());
+            address.setStreet(request.getAddress().getStreet());
+            address.setZipcode(request.getAddress().getZipcode());
+
+            user.setAddress(address);
+        }
+
+        return user;
+    }
+
+    private UserResponse mapToUserResponse(User user){
+        UserResponse response = new UserResponse();
+        response.setId(String.valueOf(user.getId()));
+        response.setFirstName(user.getFirstName());
+        response.setLastName(user.getLastName());
+        response.setEmail(user.getEmail());
+        response.setPhone(user.getPhone());
+        //response.setRole(user.getRole());
+
+        if(user.getAddress() !=null){
+            AddressDto addressDto = new AddressDto();
+            addressDto.setCountry(user.getAddress().getCountry());
+            addressDto.setState(user.getAddress().getState());
+            addressDto.setCity(user.getAddress().getCity());
+            addressDto.setStreet(user.getAddress().getStreet());
+            addressDto.setZipcode(user.getAddress().getZipcode());
+            response.setAddress(addressDto);
+        }
+        return response;
     }
 }
